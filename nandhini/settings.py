4fd79b300d8cbfa2edb84a23ad1da4cd35f95d48
Karthik <developer4python@gmail.com>
2024-10-01 15:52:37 +0530

    first commit

--- nandhini/settings.py
+++ nandhini/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'nandhini.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -84,6 +85,13 @@ DATABASES = {
         "PASSWORD": "admin",
         "HOST": "localhost",
         "PORT": "5432",
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'nand_live.sqlite3'
     }
 }
 
